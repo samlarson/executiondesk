4d58696b3d882078c869103b1d188edb40b28a65
samlarson <slarson207@gmail.com>
2018-07-19 09:42:09 -0400

    Request handling for dataframe support
    Added functions to write to csv files and converted return calls in
    existing functions to a dataframe object

--- alphavantageAPI.py
+++ alphavantageAPI.py
@@ -1,7 +1,19 @@
-import requests
+import os
+import io
+import sys
 import csv
+import json
+import requests
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from urllib.request import urlopen
+from pandas.io.json import json_normalize
+
 
 api_key = "FFK3WVR52378LU0Q"
+url = "https://www.alphavantage.co/query"
+
 
 # Returns intraday time series (timestamp, open, high, low, close, volume) of
 # the equity specified, updated realtime.
@@ -13,14 +25,16 @@ api_key = "FFK3WVR52378LU0Q"
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
-def getDataIntraDay (symbol, interval, datatype, api_key):
+def getDataIntraDay(symbol, interval, datatype, api_key):
     data = { "function": "TIME_SERIES_INTRADAY",
-             "interval":interval,
+             "interval": interval,
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns daily time series (date, daily open, daily high, daily low, daily close,
 # daily volume) of the equity specified, covering up to 20 years of historical data.
@@ -30,13 +44,15 @@ def getDataIntraDay (symbol, interval, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
-def getDataDaily (symbol, datatype, api_key):
+def getDataDaily(symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_DAILY",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response =  requests.get(url, params = data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns daily time series (date, daily open, daily high, daily low, daily close,
 # daily volume, daily adjusted close, and split/dividend events) of the equity specified,
@@ -47,13 +63,15 @@ def getDataDaily (symbol, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
 def getDataDailyAdjusted (symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_DAILY_ADJUSTED",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns weekly time series (last trading day of each week, weekly open, weekly high,
 # weekly low, weekly close, weekly volume) of the equity specified, covering up to 20
@@ -64,13 +82,15 @@ def getDataDailyAdjusted (symbol, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
 def getDataWeekly (symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_WEEKLY",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns weekly adjusted time series (last trading day of each week, weekly open,
 # weekly high, weekly low, weekly close, weekly adjusted close, weekly volume,
@@ -81,13 +101,15 @@ def getDataWeekly (symbol, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
 def getDataWeeklyAdjusted (symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_WEEKLY_ADJUSTED",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns monthly time series (last trading day of each month, monthly open,
 # monthly high, monthly low, monthly close, monthly volume) of the equity
@@ -98,13 +120,15 @@ def getDataWeeklyAdjusted (symbol, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
 def getDataMonthly (symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_MONTHLY",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Returns monthly adjusted time series (last trading day of each month, monthly
 # open, monthly high, monthly low, monthly close, monthly adjusted close,
@@ -116,19 +140,20 @@ def getDataMonthly (symbol, datatype, api_key):
 #           json returns the time series in JSON format; csv returns the time
 #           series as a CSV (comma seperated value) file.
 # api_key:  AlphaVantage API Key
-
 def getDataMonthlyAdjusted (symbol, datatype, api_key):
     data = { "function": "TIME_SERIES_MONTHLY_ADJUSTED",
              "symbol": symbol,
              "datatype": datatype,
              "apikey": api_key}
-    return requests.get("https://www.alphavantage.co/query", params = data)
+    url_response = requests.get(url, params=data).content
+    df_response = pd.read_csv(io.StringIO(url_response.decode('utf-8')))
+    return df_response
+
 
 # Prints contents of csv file onto terminal / IDE.
-
 # data: csv file returned from any one of the methods above.
-
-def printData (data):
+# TODO: this can probably be deleted because we can print from createFrame or any above functions
+def printData(data):
     with requests.Session() as s:
         data_csv = data
 
@@ -139,7 +164,44 @@ def printData (data):
         for row in my_list:
             print(row)
 
-#---TEST METHOD---#
 
+# TODO: handle potential csv/json calls from main method; possibly remove "json_normalize" method on dataframe call
+def createFrame(data):
+    # json_data = data.text
+    # print(json_data)
+    # x = json.loads(json_data)
+    # # df = pd.DataFrame(pd.io.json.json_normalize(x))
+    # df = pd.DataFrame(x)
+    # df.drop(df.index[:2], inplace=True)
+    # df.drop(df.index[-3:], inplace=True)
+    # #print(df["Time Series (Daily)"])
+    # #df.plot(x='Time Series (Daily)', y='low', style='o')
+
+    csv_data = data.text
+    writeCSV(data, "csv_data.csv")
+    df = pd.read_csv("csv_data.csv")
+    print(df)
+
+
+# TODO: refactor "fieldname" input for dynamic input_data; separate bottom statement into a readCSV function
+# We don't need this to transform API requests to pandas dataframes but we can use it later when we need to write csv's
+def writeCSV(input_data, out_file):
+    if os.path.isfile("csv_data.csv"):
+        os.remove("csv_data.csv")
+
+    with open(out_file, "w") as file:
+        writer = csv.DictWriter(file, fieldnames=["Timestamp", "Open", "High", "Low", "Close", "Volume"])
+        writer.writeheader()
+        with open(input_data, "r") as f:
+            reader = csv.reader(f)
+            writer.writerows({'Date': row[0], 'Open': row[1]}
+                            for row in reader
+                            )
+
+
+# TODO: create a separate main.py file for better organization
+#---TEST METHOD---#
 dailyAAPL = getDataDaily("AAPL", "csv", api_key)
-printData(dailyAAPL)
+print(dailyAAPL)
+# printData(dailyAAPL)
+# createFrame(dailyAAPL)
